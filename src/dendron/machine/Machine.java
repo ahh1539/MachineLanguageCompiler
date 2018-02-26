067381faabb621d06d81be51d05168f7b99a62c5
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-02-25 19:24:22 -0500

    part one almost done

--- src/dendron/machine/Machine.java
+++ src/dendron/machine/Machine.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 import java.util.Map;
 import java.util.HashMap;
 import dendron.Errors;
+import java.lang.Math;
 
 /**
  * An abstraction of a computing machine that reads instructions
@@ -143,6 +144,135 @@ public class Machine {
             return "STORE " + this.name;
         }
     }
+
+
+
+    public static class Divide implements Instruction{
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 / op2 );
+        }
+
+        @Override
+        public String toString() {
+            return "DIVIDE";
+        }
+
+    }
+
+    public static class Subtract implements Instruction{
+
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 - op2 );
+        }
+
+        @Override
+        public String toString() {
+            return "SUBTRACT";
+        }
+
+    }
+
+    public static class SquareRoot implements Instruction{
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            double op1 = Math.sqrt(op2);
+            int op3 = (int)op1;
+            stack.push(op3);
+        }
+
+        @Override
+        public String toString() {
+            return "SQUAREROOT";
+        }
+
+    }
+
+    public static class PushConst implements Instruction{
+        private int constant;
+
+        public PushConst(int constant){
+            this.constant = constant;
+        }
+        @Override
+        public void execute() {
+            stack.push(constant);
+        }
+        @Override
+        public String toString() {
+            return "PUSHCONST";
+        }
+
+    }
+
+    public static class Print implements Instruction{
+        @Override
+        public void execute() {
+            int op1 = stack.pop();
+            System.out.println("***" + op1);
+        }
+        @Override
+        public String toString() {
+            return "PRINT";
+        }
+
+    }
+
+    public static class Negate implements Instruction{
+        @Override
+        public void execute() {
+            int op1 = stack.pop();
+            int op2 = (op1 * -1);
+            stack.push(op2);
+        }
+
+        @Override
+        public String toString() {
+            return "NEGATE";
+        }
+
+    }
+
+    public static class Multiply implements Instruction{
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 * op2 );
+        }
+        @Override
+        public String toString() {
+            return "MULTIPLY";
+        }
+
+    }
+
+    public static class Load implements Instruction{
+        private String ident;
+
+        public Load(String ident){
+            this.ident = ident;
+        }
+
+        @Override
+        public void execute() {
+            stack.push(table.get(ident));
+
+        }
+        @Override
+        public String toString() {
+            return "LOAD";
+        }
+
+
+    }
+
 
     //
     // ENTER YOUR CODE FOR THE OTHER INSTRUCTION CLASSES HERE.
