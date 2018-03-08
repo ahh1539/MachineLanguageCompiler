78ad0fa7bfd93da87049da11c3c56963be563364
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-03-08 01:49:57 -0500

    fully completed print class

--- src/dendron/tree/Print.java
+++ src/dendron/tree/Print.java
@@ -2,20 +2,46 @@ package dendron.tree;
 
 import dendron.machine.Machine;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/*
+Name: Alexander Hurley
+Date: 3/8/2018
+ */
 public class Print implements ActionNode{
 
+    private ExpressionNode printee;
+    /*
+    initialized print node
+     */
     public Print(ExpressionNode printee){
-
+        this.printee = printee;
     }
-
+    /*
+    This prints out the evaluated printee expression node
+     */
     public void execute(java.util.Map<String,Integer> symTab){
-
+        System.out.println(this.printee.evaluate(symTab) + "===");
     }
+
+    /*
+    prints out user code
+     */
     public void infixDisplay(){
-
+        System.out.println("Print");
+        printee.infixDisplay();
     }
-    public java.util.List<Machine.Instruction> emit(){
 
+    /*
+    This method returns the code emitted by the printee node that pushes the
+    value of the printee expression onto the stack
+     */
+    public java.util.List<Machine.Instruction> emit(){
+        Machine.Print printt = new Machine.Print();
+        ArrayList<Machine.Instruction> llist = new ArrayList<Machine.Instruction>();
+        llist.add(printt);
+        return llist;
     }
 
 
