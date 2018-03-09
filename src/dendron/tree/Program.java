f25b47cc4ebceda24d756601bdfe91139d615eb8
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-03-09 02:26:31 -0500

    completely finished with program

--- src/dendron/tree/Program.java
+++ src/dendron/tree/Program.java
@@ -2,28 +2,52 @@ package dendron.tree;
 
 import dendron.machine.Machine;
 
+import java.util.ArrayList;
+
 /*
 Name: Alexander Hurley
 Date: 3/9/2018
  */
+
 public class Program implements ActionNode {
 
-    public Program(){
+    private ArrayList<ActionNode> ary = new ArrayList<ActionNode>();
 
-    }
-
+    /*
+    Add a child of this Program node.
+     */
     public void addAction(ActionNode newNode){
+        ary.add(newNode);
 
     }
 
+    /*
+    Execute each ActionNode in this object
+     */
     public void execute(java.util.Map<String,Integer> symTab){
-
+        for (ActionNode thing:ary) {
+            thing.execute(symTab);
+        }
     }
+
+    /*
+    Show the infix displays of all children
+     */
     public void infixDisplay(){
+        for (ActionNode thing: ary){
+            thing.infixDisplay();
+        }
 
     }
 
+    /*
+    Create a list of instructions emitted by each child
+     */
     public java.util.List<Machine.Instruction> emit(){
-
+        ArrayList<Machine.Instruction> llist = new ArrayList<Machine.Instruction>();
+        for (ActionNode thing: ary) {
+            llist.addAll(thing.emit());
+        }
+        return llist;
     }
 }
